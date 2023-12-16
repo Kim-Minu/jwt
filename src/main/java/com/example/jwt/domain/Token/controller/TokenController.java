570749c386694d21a9ecc136efa040772a05a627
minwoo <minu0922@gmail.com>
2023-12-16 22:49:03 +0900

    exception handler 추가

--- src/main/java/com/example/jwt/domain/Token/controller/TokenController.java
+++ src/main/java/com/example/jwt/domain/Token/controller/TokenController.java
@@ -8,14 +8,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/token")
 public class TokenController {
     private final TokenService tokenService;
 
-    @PostMapping("/api/token")
+    @PostMapping("")
     public ResponseEntity<CreateAccessTokenResponseDto> createNewAccessToken(@RequestBody CreateAccessTokenRequestDto requestDto) {
         return new ResponseEntity<>(tokenService.createNewAccessToken(requestDto), HttpStatus.CREATED);
     }
